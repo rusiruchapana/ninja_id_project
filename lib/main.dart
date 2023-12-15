64d662ab1f8a72824dd9dab2c7eeaee6b81083da
Rusiru_Chapana <rusiruchapana@gmail.com>
2023-12-15 15:00:10 +0000

    Floating action button create.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -7,82 +5,98 @@ void main() => runApp(MaterialApp(
       home: Display(),
     ));
 
-class Display extends StatelessWidget {
+class Display extends StatefulWidget {
   const Display({Key? key}) : super(key: key);
+
+  @override
+  State<Display> createState() => _DisplayState();
+}
+
+class _DisplayState extends State<Display> {
+  int level = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0x23000000),
-      appBar: AppBar(
-        backgroundColor: Color(0xff3b3535),
-        title: Text('Ninja ID Card'),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 40, 30, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/ninja icon.png'),
-                radius: 50.0,
-              ),
-            ),
-            SizedBox(height: 30),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2,
-              ),
-            ),
-            SizedBox(height: 3),
-            Text(
-              'RUSIRU',
-              style: TextStyle(
-                color: Colors.amber,
-                letterSpacing: 2,
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 30),
-            Text(
-              'CURRENT NINJA LEVEL',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2,
-              ),
-            ),
-            SizedBox(height: 3),
-            Text(
-              '8',
-              style: TextStyle(
-                color: Colors.amber,
-                letterSpacing: 2,
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 30),
-            Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.white70,
-                ),
-                SizedBox(width: 10),
-                Text(
-                  'rusiruchapana@gmail.com',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                )
-              ],
-            )
-          ],
+        backgroundColor: Color(0x23000000),
+        appBar: AppBar(
+          backgroundColor: Color(0xff3b3535),
+          title: Text('Ninja ID Card'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(30, 40, 30, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/ninja icon.png'),
+                  radius: 50.0,
+                ),
+              ),
+              SizedBox(height: 30),
+              Text(
+                'NAME',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(height: 3),
+              Text(
+                'RUSIRU',
+                style: TextStyle(
+                  color: Colors.amber,
+                  letterSpacing: 2,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 30),
+              Text(
+                'CURRENT NINJA LEVEL',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(height: 3),
+              Text(
+                '$level', // Display the current level
+                style: TextStyle(
+                  color: Colors.amber,
+                  letterSpacing: 2,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: [
+                  Icon(
+                    Icons.email,
+                    color: Colors.white70,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'rusiruchapana@gmail.com',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              level++;
+            });
+          },
+          child: Icon(
+            Icons.add,
+          ),
+        ));
   }
 }
